1df18c0b53c83882b848e896ae82c244452978e5
Sheharyar Iftikhar <sheharyar.isys@gmail.com>
2024-07-14 08:41:10 -0700

    vue 2 router

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
   </div>
   <div class="w-full h-[90vh] flex justify-center items-center">
     <div class="w-full h-[50vh] text-white bg-slate-500 3xl:bg-black 3xl:w-[50%] 3xl:h-[50vh] transition flex justify-center items-center">
-      <h1>Vue-JS 2 starter template</h1>
+      <h1>How's the Weather Now!</h1>
+      <RouterView></RouterView>
     </div>
   </div>
   </div>
@@ -14,6 +15,12 @@
 
 <script setup>
 import TopNavigation from "./components/TopNavigation.vue";
+import { VueRouter } from "vue-router";
+
+
+vue.use(VueResourse);
+vue.use(VueRouter);
+
 
 </script>
 
